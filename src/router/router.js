d4c9232d2a740f2d99c3a759f1bb53ed1715936a
Aphans <rapperanqeel@hotmail.com>
2023-02-08 23:54:32 +0100

    adding layout

--- src/router/router.js
+++ src/router/router.js
@@ -2,21 +2,46 @@ import {createRouter,createWebHashHistory} from 'vue-router'
 
 
 const routes = [
-    { path: '/', component: ()=>import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage')},
-    { path: '/about', component: ()=>import(/*webpackChunkName: "AboutPage" */'../modules/pokemon/pages/AboutPage')},
-    { 
-      path: '/:id',
-      name:'pokemon-id',
-      component: ()=>import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
-      props:(route)=>{
-        const id = Number(route.params.id)
-        return isNaN(id)?{id:1}:{id:Number(id)
-        }
-      },
+    {
+      path:'/',
+      redirect:'/home'
     },
-    { path: '/:pathMatch(.*)*', component: ()=>import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')}
-  ]
-
+    {
+      path: '/pokemon',
+      name: 'pokemon',
+      component: () => import(/* webpackChunkName: "PokemonLayout" */ '@/modules/pokemon/layouts/PokemonLayout'),
+      children: [
+          { 
+              path: 'home', 
+              name: 'pokemon-home',
+              component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage'),
+          },
+          { 
+              path: 'about', 
+              name: 'pokemon-about',
+              component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage')
+          },
+          { 
+              path: 'pokemonid/:id',
+              name: 'pokemon-id',
+              component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'),
+              props: ( route ) => {
+                  const id = Number( route.params.id );
+                  return isNaN( id ) ? { id: 1 } : { id }
+              }
+          },
+          {
+              path: '',
+              redirect: { name: 'pokemon-about' }
+          },
+      ]
+  },
+   
+          { 
+             path: '/:pathMatch(.*)*', 
+             component: ()=>import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')
+          },
+]
 const router = createRouter({
     history: createWebHashHistory(),
     routes, // short for `routes: routes`
