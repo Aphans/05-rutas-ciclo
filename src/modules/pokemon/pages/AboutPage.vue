47f4beba8d863ad05979d2c97cf670f108eab090
Aphans <rapperanqeel@hotmail.com>
2023-02-08 09:45:27 +0100

    adding some fix

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,23 @@
 <template>
     <h1>About Page</h1>
 </template>
+
+<script>
+export default {
+    setup(){
+        
+    },
+    beforeCreate(){},
+    created(){},
+    beforeMount(){},
+    mounted(){},
+    updated(){},
+    activated(){},
+    desactivated(){},
+    beforeUnmount(){},
+    unmounted(){},
+    errorCaptured(){},
+    renderTracked(){},
+    renderTriggered(){}
+}
+</script>
--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Pokemon Page</h1>
+    <h1>Pokemon: <span>#{{ id }}</span></h1>
 </template>
+
+<script>
+export default{
+    props:{
+         id:{
+             type:Number,
+             required:true
+         }
+    },
+    data(){
+        return{
+            // id: this.$route.params.id,
+            pokemon:null
+        }
+    },
+    created(){
+        //const {id} = this.$route.params
+       this.getPokemon()
+    },
+    methods:{
+        async getPokemon(){
+            try{
+            const pokemon = await fetch (`https://pokeapi.co/api/v2/pokemon/${this.id}`).then( r=>r.json())
+            console.log(pokemon)
+            this.pokemon = pokemon
+            }
+            catch(error){
+                this.$router.push('/')
+                console.log('No hay nada que hacer aquí')
+            }
+
+        }
+    }
+
+}
+</script>
